6b75fb31591fd96949e85eaf7c8f70b8d91ac239
henningholm <henningholm90@gmail.com>
2023-03-22 14:09:55 +0100

    Add some basic stuff

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,2 @@
+<h1>My blog</h1>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/hLQl3WQQoQ0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
